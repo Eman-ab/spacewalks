7e5e61c208f953b74a54da2b158b50018859c9f0
eman <emanabdelrhman2@gmail.com>
2025-02-25 07:03:04 +0000

    Move import statements to the top of the script

--- eva_data_analysis.py
+++ eva_data_analysis.py
@@ -1,67 +1,62 @@
+import json
+import csv
+import datetime as dt
 import matplotlib.pyplot as plt
-import pandas as pd
 
+# https://data.nasa.gov/resource/eva.json (with modifications)
+data_f = open('./eva-data.json', 'r')
+data_t = open('./eva-data.csv','w')
+g_file = './cumulative_eva_graph.png' 
 
-def read_json_to_dataframe(input_file):
-    """
-    Read the data from a JSON file into a Pandas dataframe.
-    Clean the data by removing any incomplete rows and sort by date
+fieldnames = ("EVA #", "Country", "Crew    ", "Vehicle", "Date", "Duration", "Purpose")
 
-    Args:
-        input_file_ (str): The path to the JSON file.
+data=[]
 
-    Returns:
-         eva_df (pd.DataFrame): The cleaned and sorted data as a dataframe structure
-    """
-    print(f'Reading JSON file {input_file}')
-    # Read the data from a JSON file into a Pandas dataframe
-    eva_df = pd.read_json(input_file, convert_dates=['date'])
-    eva_df['eva'] = eva_df['eva'].astype(float)
-    # Clean the data by removing any incomplete rows and sort by date
-    eva_df.dropna(axis=0, inplace=True)
-    eva_df.sort_values('date', inplace=True)
-    return eva_df
+for i in range(374):
+    line=data_f.readline()
+    print(line)
+    data.append(json.loads(line[1:-1]))
+#data.pop(0)
+## Comment out this bit if you don't want the spreadsheet
 
+w=csv.writer(data_t)
 
-def write_dataframe_to_csv(df, output_file):
-    """
-    Write the dataframe to a CSV file.
+time = []
+date =[]
 
-    Args:
-        df (pd.DataFrame): The input dataframe.
-        output_file (str): The path to the output CSV file.
+j=0
+for i in data:
+    print(data[j])
+    # and this bit
+    w.writerow(data[j].values())
+    if 'duration' in data[j].keys():
+        tt=data[j]['duration']
+        if tt == '':
+            pass
+        else:
+            t=dt.datetime.strptime(tt,'%H:%M')
+            ttt = dt.timedelta(hours=t.hour, minutes=t.minute, seconds=t.second).total_seconds()/(60*60)
+            print(t,ttt)
+            time.append(ttt)
+            if 'date' in data[j].keys():
+                date.append(dt.datetime.strptime(data[j]['date'][0:10], '%Y-%m-%d'))
+                #date.append(data[j]['date'][0:10])
 
-    Returns:
-        None
-    """
-    print(f'Saving to CSV file {output_file}')
-    # Save dataframe to CSV file for later analysis
-    df.to_csv(output_file, index=False)
+            else:
+                time.pop(0)
+    j+=1
 
+t=[0]
+for i in time:
+    t.append(t[-1]+i)
 
-# Main code
+date,time = zip(*sorted(zip(date, time)))
 
-print("--START--")
-
-input_file = open('./eva-data.json', 'r')
-output_file = open('./eva-data.csv', 'w')
-graph_file = './cumulative_eva_graph.png'
-
-# Read the data from JSON file
-eva_data = read_json_to_dataframe(input_file)
-
-# Convert and export data to CSV file
-write_dataframe_to_csv(eva_data, output_file)
-
-print(f'Plotting cumulative spacewalk duration and saving to {graph_file}')
-# Plot cumulative time spent in space over years
-eva_data['duration_hours'] = eva_data['duration'].str.split(":").apply(lambda x: int(x[0]) + int(x[1])/60)
-eva_data['cumulative_time'] = eva_data['duration_hours'].cumsum()
-plt.plot(eva_data['date'], eva_data['cumulative_time'], 'ko-')
+plt.plot(date,t[1:], 'ko-')
 plt.xlabel('Year')
 plt.ylabel('Total time spent in space to date (hours)')
 plt.tight_layout()
-plt.savefig(graph_file)
+plt.savefig(g_file)
 plt.show()
 
-print("--END--")
+
